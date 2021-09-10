ef7ac2183158e30875b9dbf4029df1a103ef2d0c
TTatchell <tim.tatchell24@gmail.com>
2021-09-10 18:50:22 +1000

    upload

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,5 +2,13 @@ class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
 
+  def city_openings(date_1, date_2)
+    byebug
+    City.listings.all.select do |instance|
+      byebug
+      instance.created_at.to_date < date_1.to_date && instance.created_at.to_date > date_2.to_date
+    end
+  end
 end
 
+#e
